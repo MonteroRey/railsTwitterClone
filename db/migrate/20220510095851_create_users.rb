2d58d834c898b3b9381bae569fbee61f1573d1a1
John Doe <johndoe@example.com>
2022-05-10 18:35:18 +0800

    commit 2

--- db/migrate/20220510095851_create_users.rb
+++ db/migrate/20220510095851_create_users.rb
@@ -2,10 +2,10 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name, null: false
-      t.string :email, null: false , index:{ unique: true}
+      t.string :email, null: false, index:{ unique: true}
       t.string :slug,  null: false, index: {unique: true}
-      t.string :password_digest  , null: false
-      t.string :remember_digest , index: true
+      t.string :password_digest, null: false
+      t.string :remember_digest, index: true
       t.timestamps
     end
   end
