aa8d3b90947f577ab5d33b01fcd64aa7551e4801
John Doe <johndoe@example.com>
2022-05-11 17:43:40 +0800

    commit 13 : add 'has_many :tweets, dependent: :destroy'

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     #attr_accessible :email, :name, :password, :password_confirmation
     # attr_accessor :password, :password_confirmation
     has_secure_password
+    has_many :tweets, dependent: :destroy
 end
