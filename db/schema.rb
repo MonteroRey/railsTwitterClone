913c9371a08a3ee63bdcbad11cc41707d68fb53c
John Doe <johndoe@example.com>
2022-05-11 17:46:37 +0800

    commit 15: tweet table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_10_095851) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_11_092845) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tweets", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
