4eaa43f6be28bfea53a73bc2d02b2501300366b9
John Doe <johndoe@example.com>
2022-05-12 17:03:03 +0800

    commit 27: add forget and remember method in the session helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,4 +16,14 @@ module SessionsHelper
           end
         end
     end
+    def forget(user)
+      user.forget
+      cookies.delete(:user_id)
+      cookies.delete(:remember_token)
+    end
+    def remember(user)
+      user.remember
+      cookies.permanent.encrypted[:user_id] = user.id
+      cookies.permanent[:remember_token] = user.remember_token
+    end
 end
