63e5b2fd3fa35c8bf5c45befb28071ac59a77eea
John Doe <johndoe@example.com>
2022-05-12 17:04:33 +0800

    commit 29: add remember and forget method in users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
     # attr_accessor :password, :password_confirmation
     has_secure_password
     has_many :tweets, dependent: :destroy
+    def remember
+        self.remember_token = self.class.new_token
+        update_attribute(:remember_digest, self.class.digest(remember_token))
+    end
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
 end
