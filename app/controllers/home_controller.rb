be96f075ef0b2b5e0daca47759d5048010529fbb
John Doe <johndoe@example.com>
2022-05-12 15:16:19 +0800

    commit 24: add index method in the home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,7 @@
 class HomeController < ApplicationController
+    #before_action :logged_in_user
+
+  def index
+    #@tweets = current_user.feed.includes(:user).with_attached_image.paginate(page: params[:page])
+  end
 end
