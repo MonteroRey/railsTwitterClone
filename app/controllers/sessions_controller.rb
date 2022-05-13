7e2e963524b1a0b15b1c38c3a39ee578a335092b
John Doe <johndoe@example.com>
2022-05-13 13:40:52 +0800

    commit 40 : destroy method for session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,9 +19,14 @@ class SessionsController < ApplicationController
         redirect_to login_path, flash: { danger: "Please sign in." }
       end
     end
-    
+
     def logged_in?
       !current_user.nil?
+    end
+################################ log out ########
+    def destroy
+      log_out if logged_in?
+      redirect_to root_url
     end
 
     
