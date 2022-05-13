628b6b170da378adfa9a8763e50b617aeea81e4a
John Doe <johndoe@example.com>
2022-05-13 16:43:48 +0800

    commit 55: add inline_svg gem

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ gem "bootsnap", require: false
 gem "webpacker"
 gem "will_paginate-bootstrap-style"
 gem "will_paginate"
+gem "inline_svg" 
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   get    "signup", to: "users#new"  #to routes into users/new.html.erb
   resources :users, only: [:index, :show, :edit, :create, :update, :destroy]
   resources :tweets,       only: [:index, :create, :destroy]
+
+  get "help",  to: "static_pages#help"   ###to render in the footer in the layout 
+  get "about", to: "static_pages#about"  ###to render in the footer in the layout 
   # Defines the root path route ("/")
   # root "articles#index"
 end
