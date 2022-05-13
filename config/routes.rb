2fb6b06036cdf79dbb4ec7b9ba3df4d13ab2c8f0
John Doe <johndoe@example.com>
2022-05-13 18:41:54 +0800

    commit 59 : clean

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   get    "home",   to: "home#index"
   get    "login",  to: "sessions#new"
   post   "login",  to: "sessions#create"
-  #delete "logout", to: "sessions#destroy"
+  delete "logout", to: "sessions#destroy"
   get "logout", to: "sessions#destroy"
   get    "signup", to: "users#new"  #to routes into users/new.html.erb
   resources :users, only: [:index, :show, :edit, :create, :update, :destroy]
-  resources :tweets,       only: [:index, :create, :destroy]
+  resources :tweets, only: [:index, :create, :destroy]
 
   get "help",  to: "static_pages#help"   ###to render in the footer in the layout 
   get "about", to: "static_pages#about"  ###to render in the footer in the layout 
