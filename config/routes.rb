9534505a8217ef5f9c924d595514b9c9f176b49e
John Doe <johndoe@example.com>
2022-05-13 13:45:55 +0800

    commit 43 : add logout to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get    "home",   to: "home#index"
   get    "login",  to: "sessions#new"
   post   "login",  to: "sessions#create"
+  #delete "logout", to: "sessions#destroy"
+  get "logout", to: "sessions#destroy"
   get    "signup", to: "users#new"  #to routes into users/new.html.erb
   resources :users, only: [:index, :show, :edit, :create, :update, :destroy]
   resources :tweets,       only: [:index, :create, :destroy]
