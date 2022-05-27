dfb737cfc927ef0cfd0d29f745563344f812a8d9
John Doe <johndoe@example.com>
2022-05-27 14:05:59 +0800

    commit 80 : add validation for image in tweet model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,8 +6,11 @@ class Tweet < ApplicationRecord
 
     ################ comments ###################
     has_many :comments, dependent: :destroy
-
+    byebug
     has_one_attached :image  #to render image in the tweets controller
+
+    validates :image, content_type: ["png", "jpeg"]
+
     def display_image
         image.variant(resize_to_limit: [700, 700])
     end
